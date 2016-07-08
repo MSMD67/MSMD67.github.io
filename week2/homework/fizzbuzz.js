7d16dc88343e5dc7de8a477b35c346bcc8f7a9d2
prstf61@gmail.com <dnlp200@gmail.com>
2016-07-08 14:14:47 -0400

    Add file for fizzbuzz homework

--- week2/homework/fizzbuzz.js
+++ week2/homework/fizzbuzz.js
@@ -21,16 +21,47 @@
 // Construct a for loop that iterates through, and `console.log()`'s out, numbers 1 - 100.
 
 
+// for(num =1; num <= 100; num++){
+//   console.log(num);
+// }
+
+
 
 // ##### Step 2:
 // Add an `if/else` statement that logs the string `"fizz"` if the value being iterated over is divisible by `3`; otherwise, log out the value.
 
 
+// for(num = 1; num <=100; num++){
+//   if (num % 3 === 0){
+//     console.log('fizz');
+//   }
+// }
 
 // ##### Step 3:
 // Add an `else if` clause that logs the string `"buzz"` if the value being iterated over is divisible by `5`.
 
+// for(num = 1; num <=100; num++){
+//   if (num % 3 === 0){
+//     console.log('fizz');
+//   } else if (num % 5 ===) {
+//     console.log('buzz');
+//   }
+// }
 
 
 // ##### Step 4:
 // Add an additional `else if` clause that logs the string `"fizzbuzz"` if the value being iterated over is divisible by both `3` and `5`.
+
+
+for(num = 1; num <=100; num++){
+    if ((num % 5 === 0) && (num % 3 === 0)){
+      console.log("FizzBuzz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else if (num % 3 === 0){
+      console.log("Fizz");
+    } else {
+      console.log(num);
+    }
+      
+};
