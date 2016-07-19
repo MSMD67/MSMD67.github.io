9b67c7ab565261d3cd04f8b1f963fb4e84962e9c
prstf61@gmail.com <dnlp200@gmail.com>
2016-07-19 18:02:47 -0400

    Add new app and README file to hubot project folder

--- hubot_project/scripts/app.js
+++ hubot_project/scripts/app.js
@@ -7,15 +7,20 @@ module.exports = function(robot) {
     return res.send('meoooowwwww');
   });
 
-  robot.respond(/how are you?/i, function(res) {
+  robot.hear(/how are you?/i, function(res) {
     return res.send('I am feeling super');
   });
 
   robot.hear(/my name is (.*)/i, function(res){
-    console.log(res.match);
+    // console.log(res.match);
     var name = res.match[1];
     return res.reply('Hello, ' + name);
   });
+
+  robot.hear(/what is your name?|what's your name/i, function(res){
+    return res.send("My name is HP.  It's short for Hubot Project.");
+  });
+
 
   robot.hear(/add (.*) and (.*)/i, function(res){
       var x = parseInt(res.match[1]);
@@ -25,36 +30,78 @@ module.exports = function(robot) {
       return res.send(x + " + " + y + " = " + sum);
   });
 
-  robot.hear(/is (.*) your favorite film?/i, function (res) {
-      var film = res.match[1];
-
-      if (film === 'point break' || film === 'Point Break') {
-        return res.send('YES!');
-      } else {
-        return res.send('NO!');
-      }
+  robot.hear(/what is today's date?|what's today's date?/i, function (res) {
+      var date = new Date();
+      console.log(date);
+ 
   });
 
-    robot.respond(/What's your favorite food?/i, function(res) {
+    robot.hear(/What is your favorite food?|What's your favorite food?/i, function(res) {
     return res.send("I'm a robot; I don't eat food!");
 
   });
 
-      robot.hear(/(.*) to (.*)/i, function(res){
-        console.log(res);
+      robot.hear(/Can you print numbers from (.*) to (.*)?/i, function(res){
+        // console.log(res);
         var num1 = parseInt(res.match[1]);
         var num2 = parseInt(res.match[2]);
-        var output = "";
-        console.log(num1,num2);
-      // for (var i = num1;  i<=num2; i++){
-      //       output += 
-      //       console.log(output);
-      //     }
-      var number, output=[];
-      for (var i = num1; i <= num2; i++) {
-          output.push(i);
+        var output = [];
+        // console.log(num1,num2);
+      for (var i = num1;  i<=num2; i++){
+          if (i === num2){
+            output.push(i);
+          } else {
+            output.push(i + ",");
+          }
       }
       console.log.apply(console, output);
+
   });
+        
+      robot.hear(/I am entering 5 integers in random order with a space between each number: (.*) (.*) (.*) (.*) (.*)/i,
+        function (res) {
+            var num1 = parseInt(res.match[1]);
+            var num2 = parseInt(res.match[2]);
+            var num3 = parseInt(res.match[3]);
+            var num4 = parseInt(res.match[4]);
+            var num5 = parseInt(res.match[5]);
+           // console.log(num1, num2, num3, num4, num5);
+            var array = [num1, num2, num3, num4, num5];
+            // var final = [];
+            var min, max;
+            var bigSmall = function (array){
+
+                var minValue = function () {
+                  var min = array[0];
+                    for (var i=0; i<array.length; i++) {
+                      if (array[i]<min) {
+                      min = array[i];
+                      }
+                    }
+                    
+                    // final.push(min);
+                    console.log("The minimum value is: " + min);
+                    return min;
+                };
+                
+                var maxValue = function () {
+                  var max = array[0];
+                    for (var i=0; i<array.length; i++) {
+                      if (array[i]>max) {
+                        max = array[i];
+                        }
+                      }
+                    // final.push(max);
+                    console.log("The maximum value is: " + max);
+                    return max;
+                };
+                      
+    minValue();
+    maxValue();
+    // console.log(final);
+    };
+    bigSmall(array);         
+  });
+
 
 };
